a4f1cd220f486ab6b6486f5d5b4983f28d8b312a
Natalie Mona Matthews <natalie.mona@gmail.com>
2019-11-24 15:52:03 -0500

    label everything

--- llvm-iterator.cpp
+++ llvm-iterator.cpp
@@ -3,11 +3,19 @@
 #include <unistd.h>
 #include <string.h>
 
+#include <map>
+#include <utility>
+
 #include <llvm-c/Core.h>
 #include <llvm-c/BitReader.h>
 #include <llvm-c/BitWriter.h>
 
 #define BUFSIZE 1000
+
+using namespace std;
+
+static map<long, int> labels;
+static map<long, int> stupid_map;
 
 void print_operand_info(FILE* f, LLVMValueRef instruction) {
     fprintf(f, "[");
@@ -16,25 +24,38 @@ void print_operand_info(FILE* f, LLVMValueRef instruction) {
     for (int i = 0; i < n_operands; i++) {
         fprintf(f, "{");
         LLVMValueRef op = LLVMGetOperand(instruction, i);
-        if (LLVMIsAConstant(op)) {
+        LLVMValueRef constant = LLVMIsAConstant(op);
+        if (constant) {
             fprintf(f, "\"isConstant\": true");
         } else {
             fprintf(f, "\"isConstant\": false");
+            fprintf(f, ",\"register\": \"var-%i\"", labels[(long) op]);
         }
 
         // // Print operand type
         // LLVMTypeKind operand_type = LLVMGetTypeKind(op);
         // printf("%s\n", LLVMPrintTypeToString(operand_type));
 
+        // LLVMConstInt ??
+
         // Print operand value
         LLVMValueKind value_kind = LLVMGetValueKind(op);
         fprintf(f, ",\"valueKind\":");
         switch (value_kind) {
+            case LLVMArgumentValueKind:
+                fprintf(f, "\"argument\"");
+                break;
+            case LLVMBasicBlockValueKind:
+                fprintf(f, "\"basic block\"");
+                fprintf(f, ",\"value\": \"block-%i\"", labels[(long) op]);
+                break;
             case LLVMConstantExprValueKind:
                 fprintf(f, "\"constant expression\"");
                 break;
             case LLVMConstantIntValueKind:
                 fprintf(f, "\"integer\"");
+                fprintf(f, ",\"zero_extended_value\": %llu", LLVMConstIntGetZExtValue(op));
+                fprintf(f, ",\"sign_extedned_value\": %llu", LLVMConstIntGetSExtValue(op));
                 break;
             case LLVMFunctionValueKind:
                 fprintf(f, "\"function\"");
@@ -88,8 +109,16 @@ void print_instruction_info(FILE* f, LLVMValueRef instruction) {
         case LLVMICmp:
             fprintf(f, "\"icmp\"");
             break;
+        case LLVMGetElementPtr:
+            fprintf(f, "\"get element ptr\"");
+            break;
         default:
             fprintf(f, "\"unrecognized opcode %i\"", opcode);
+    }
+
+    if (opcode != LLVMStore) {
+        labels.insert(pair<long, int>((long) instruction, labels.size()));
+        fprintf(f, ",\"register\": \"var-%i\"", labels[(long) instruction]);
     }
     fprintf(f, ",\"operands\":");
     print_operand_info(f, instruction);
@@ -137,12 +166,19 @@ int main(const int argc, const char *const argv[]) {
     for (LLVMValueRef fun = firstFun; fun; fun = LLVMGetNextFunction(fun)) {
         fprintf(f, "{\"blocks\":[");
 
-        // loop through basic blocks
         LLVMBasicBlockRef firstBlock = LLVMGetFirstBasicBlock(fun);
         LLVMBasicBlockRef lastBlock = LLVMGetLastBasicBlock(fun);
+
+        // Label blocks
         for (LLVMBasicBlockRef block = firstBlock; block;
             block = LLVMGetNextBasicBlock(block)) {
-                fprintf(f, "{\"label%s\":[", LLVMGetBasicBlockName(block));
+                labels.insert(pair<long, int>((long) block, labels.size()));
+        }
+
+        // Read instructions in each block
+        for (LLVMBasicBlockRef block = firstBlock; block;
+            block = LLVMGetNextBasicBlock(block)) {
+                fprintf(f, "{\"block-%i\":[", labels[(long) block]);
 
                 // loop through instructions
                 LLVMValueRef firstInstr = LLVMGetFirstInstruction(block);
